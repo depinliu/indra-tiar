0bb69f8467edac64ea5931abf834385408f28c3e
gorosei <devin@hasnurgroup.com>
2026-05-15 22:08:18 +0700

    add music auto play

--- assets/house-nQRiximH.js
+++ assets/house-nQRiximH.js
@@ -1,4 +1,4 @@
-import{c as a}from"./FloralSVG-Dw0QLm6l.js";/**
+import{c as a}from"./FloralSVG-Cu7twHdK.js";/**
  * @license lucide-vue-next v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
