2f6d17fa13a214fded42deefa1245552b468a281
gorosei <devin@hasnurgroup.com>
2026-05-16 13:32:10 +0700

    bug fixing

--- assets/loader-circle-BR-sw_P2.js
+++ assets/loader-circle-BR-sw_P2.js
@@ -1,4 +1,4 @@
-import{c as e}from"./FloralSVG-BvFJDEA5.js";/**
+import{c as e}from"./createLucideIcon-BIuKli9U.js";/**
  * @license lucide-vue-next v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
